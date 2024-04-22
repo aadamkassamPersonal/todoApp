d72841466f6aff64fefce54932be06a9dbb9f1e3
Aadam <aadamkassam@gmail.com>
2024-04-22 16:14:30 -0700

    updating app for functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+import Form from"./components/Form";
+import FilterButton from "./components/FilterButton";
+import { useState } from "react";
 
-function App() {
+
+
+function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  
+  function addTask(name) {
+    const newTask = { id: "id", name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  
+  const taskList = tasks?.map((task) => (
+  <Todo 
+    id={task.id}
+    name={task.name}
+    completed={task.completed}
+    key={task.id}
+    />));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+        <Form onSubmit={addTask}/>
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+
+      </ul>
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA}/>
   </React.StrictMode>
 );
 
